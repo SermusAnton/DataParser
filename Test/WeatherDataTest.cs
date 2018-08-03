31f4893c3ed5d1504fe06623537900e0f2fcd372
SermusAnton <antonsermus@mail.ru>
2018-08-03 22:35:59 +0300

    Add Test and WeatherDay

---
+++ Test/WeatherDataTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WeatherData;
+
+namespace Test {
+    [TestFixture]
+    public class WeatherDataTest {
+
+        [Test]
+        public void A0_InitWeatherDay() {
+            WeatherDay weatherDay = new WeatherDay();
+            Assert.IsTrue(weatherDay!= null);
+        }
+
+        [Test]
+        public void A1_GetDiffMaxMin() {
+            WeatherDay weatherDay = new WeatherDay();
+            weatherDay.Number = 1;
+            weatherDay.TempMax = 34.4;
+            weatherDay.TempMin = 15.5;
+            Assert.AreEqual(weatherDay.GetDiffMaxMin(),  18.9);
+        }
+
+    }
+}
---
+++ WeatherData/WeatherDay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherData {
+    /// <summary>
+    /// http://codekata.com/kata/kata04-data-munging/
+    /// В данных вы найдете ежедневные данные о погоде для 
+    /// Morristown, Нью-Джерси за июнь 2002 года. 
+    /// Загрузите этот текстовый файл, 
+    /// затем напишите программу для 
+    /// вывода номера дня (столбец один) 
+    /// с наименьшим температурным разбросом 
+    /// (Максимальная температура-второй столбец, 
+    /// минимальная-третий столбец).
+    /// </summary>
+    public class WeatherDay {
+
+        public int Number { get; set; }
+
+        public double TempMax { get; set; }
+
+        public double TempMin { get; set; }
+
+        public double GetDiffMaxMin() {
+            return TempMax - TempMin;
+        }
+    }
+}
