ab4c6d6605292571fa73d56b725ba9f1014ed8b9
SermusAnton <antonsermus@mail.ru>
2018-08-15 22:34:39 +0300

    Add WeatherMonth

--- Test/WeatherDataTest.cs
+++ Test/WeatherDataTest.cs
@@ -24,6 +24,51 @@ namespace Test {
             weatherDay.TempMin = 15.5;
             Assert.AreEqual(weatherDay.GetDiffMaxMin(),  18.9);
         }
+        [Test]
+        public void B0_InitWeatherMonth() {
+            WeatherMonth weatherMonth = new WeatherMonth();
+            Assert.IsTrue(weatherMonth != null);
+        }
+
+        /// <summary>
+        /// Проверяем добавление элемента в коллекцию
+        /// </summary>
+        [Test]
+        public void B1_WeatherMonthAdd() {
+            WeatherDay weatherDay = new WeatherDay();
+            weatherDay.Number = 1;
+            weatherDay.TempMax = 34.4;
+            weatherDay.TempMin = 15.5;
+
+            List<WeatherDay> days = new List<WeatherDay>();
+            days.Add(weatherDay);
+
+            WeatherMonth weatherMonth = new WeatherMonth();
+            weatherMonth.Add(weatherDay);
+            CollectionAssert.AreEqual(weatherMonth, days);
+        }
+
+        /// <summary>
+        /// Проверяем выбор минимального значения за месяц
+        /// </summary>
+        [Test]
+        public void B2_WeatherMonthAdd() {
+            WeatherDay weatherDay1 = new WeatherDay();
+            weatherDay1.Number = 1;
+            weatherDay1.TempMax = 34.4;
+            weatherDay1.TempMin = 15.5;
+
+            WeatherDay weatherDay2 = new WeatherDay();
+            weatherDay2.Number = 2;
+            weatherDay2.TempMax = 34.4;
+            weatherDay2.TempMin = 34.1;
+
+            WeatherMonth weatherMonth = new WeatherMonth();
+            weatherMonth.Add(weatherDay1);
+            weatherMonth.Add(weatherDay2);
+
+            Assert.AreEqual(weatherMonth.GetNumerDayMinDiff(), 2);
+        }
 
     }
 }
---
+++ WeatherData/WeatherMonth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherData {
+    public class WeatherMonth:  IEnumerable<WeatherDay>  {
+        private List<WeatherDay> days = new List<WeatherDay>();
+        public void Add(WeatherDay value) {
+            days.Add(value);
+        }
+
+        public IEnumerator<WeatherDay> GetEnumerator() {
+            return days.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return days.GetEnumerator();
+        }
+
+        public int GetNumerDayMinDiff() {
+            //double minDiff = days.Min(m => m.GetDiffMaxMin());
+            //return days
+            //    .Where(w=>w.GetDiffMaxMin()==minDiff)              
+            //    .Select(s => s.Number).First();
+            int number = 0;
+            int minDiff= int.MaxValue;
+            foreach (var day in days) {
+                if (day.GetDiffMaxMin() <= minDiff) {
+                    number = day.Number;
+                }                    
+            }
+            return number;
+        }
+    }
+}
